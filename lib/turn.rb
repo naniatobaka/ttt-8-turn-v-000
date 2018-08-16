c71d3e1b6e16190f5c94a80ac99581695e22546a
Ashley Wong <ashwong04@gmail.com>
2018-08-16 01:33:25 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+def display_board(board)
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts "-----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+    if (position_taken?(board, index) == false && index.between?(0, 8) == true)
+      return true
+    elsif position_taken?(board, index) == true
+      return nil
+  end
+end
+
+def position_taken?(board, index_num)
+    if board[index_num] == " " || board[index_num] == "" || board[index_num] == nil
+      return false
+    elsif board[index_num] == "X" || board[index_num] == "O"
+      return true
+  end 
+end  
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(array, index, character = "X")
+  array[index]=character
+end
+
+
+
+def turn
+
+end
